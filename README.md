e1f06f17202e9d26691573a46423964cdd728672
Hitesh <hiteshpratapsingh72@gmail.com>
2025-09-08 09:25:05 +0530

    Assignment 1

---
+++ README.md
@@ -0,0 +1,3 @@
+Assignment1:
+    Task1: To perform addition, subtraction, multiplication & division on two inputed numbers.
+    Task2: To take first name and last name as user inpur ant display a personalized message.
---
+++ assignment1/task1.py
@@ -0,0 +1,9 @@
+num1 = input('Enter the first number: ')
+num1 = int(num1)
+num2 = input('Enter the second number: ')
+num2 = int(num2)
+
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
